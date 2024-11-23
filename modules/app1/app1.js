2b00ee3e5d6a65461b91bceca675e7458c415a3a
talshpo <tal.shporen1@gmail.com>
2024-11-23 20:46:45 +0200

    edited app js

--- modules/app1/app1.js
+++ modules/app1/app1.js
@@ -1,31 +1,11 @@
-const express = require("express")
+const express = require ("express");
 const app = express();
-const dotenv = require("dotenv").config();
-const port = 3000; 
+const port = 3000;
 
-
-
-app.get("/", (req, res ) => { 
-    res.send('liel');
+app.get("/", (req, res)=>{
+    res.send("hellow world!23");
 });
 
-
-app.get("posts", (req, res ) => {
-res.send('get posts');
-});
-
-app.post("/posts", (req, res ) => {
-res.send('create new post');
-});
-
-app.put("/posts/:id", (req, res ) => {
-res.send('update post');
-});
-
-
-
-
-
-app.listen(port, ()=> {
-console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, () => {
+    console.log( 'Example app listening at http://localhost:${port}');
 });
